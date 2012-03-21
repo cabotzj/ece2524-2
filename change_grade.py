9a0aa45b72cf300facdbe7a2708d2f4a6df28931
Aldo Gomez <gomezfx@gmail.com>
2012-03-21 11:51:50 -0400

    updated change_grade.py to move algorithm into a function

--- change_grade.py
+++ change_grade.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
-
+#prints the grade twice, not sure why
 
 import re,fileinput,string
+
+def grade_changer(data, name, grade)
+	for line in data:
+		m = re.search (name, line)
+		if m:
+			print re.sub ('[%s]' % string.digits, grade, line)
+	return (data)
+
 from sys import stdin,stdout,stderr,argv
-for line in stdin:
-	m = re.search (argv[1], line)
-	if m:
-		print re.sub ('[%s]' % string.digits, argv[2], line)
-#prints the grade twice, not sure why
+
+if __name__=='__main__':
+
+	(data) = grade_changer(stdin, argv[1], argv[2])
